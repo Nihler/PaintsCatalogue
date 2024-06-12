2955aae3401ddcd76606de789e8387c7d41ffdc5
Nihler <vanilor97@gmail.com>
2024-06-12 19:27:16 +0200

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaintsListComponent } from './paints/paints-list/paints-list.component';
+import { PaintsFormComponent } from './paints/paints-form/paints-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PaintsListComponent,
+  },
+  { path: 'addPaint', component: PaintsFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
