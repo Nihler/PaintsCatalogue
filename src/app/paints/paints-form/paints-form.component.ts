624130c83fce2c98cf8ef310345528a0049d4cdf
Nihler <vanilor97@gmail.com>
2024-06-12 19:54:54 +0200

    add paint form

--- src/app/paints/paints-form/paints-form.component.ts
+++ src/app/paints/paints-form/paints-form.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-paints-form',
   templateUrl: './paints-form.component.html',
-  styleUrls: ['./paints-form.component.css']
+  styleUrls: ['./paints-form.component.css'],
 })
-export class PaintsFormComponent {
+export class PaintsFormComponent implements OnInit {
+  form: FormGroup;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      manufacturer: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      paintName: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      type: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      color: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+    });
+  }
+
+  onPaintSave() {
+    console.dir(this.form);
+  }
 }
