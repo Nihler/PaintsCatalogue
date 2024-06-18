2bb24a2a043f087ddaa7ba1b46b7ac523693ad07
Nihler <vanilor97@gmail.com>
2024-06-18 16:46:09 +0200

    header login buttons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private authListenerSubs: Subscription;
+  isLoggedIn = false;
+  constructor(private authService: AuthService) {}
 
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.getIsAuth();
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuthenticated) => {
+        this.isLoggedIn = isAuthenticated;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.authListenerSubs.unsubscribe();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
