0f03323253bc3d1ba0c9a67976c23a2d57413d0a
Nihler <vanilor97@gmail.com>
2024-07-09 15:46:28 +0200

    adding guard to profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PaintsListComponent } from './paints/paints-list/paints-list.component'
 import { PaintsFormComponent } from './paints/paints-form/paints-form.component';
 import { ProfileComponent } from './user/profile/profile.component';
 import { InventoryComponent } from './user/inventory/inventory.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -15,12 +16,13 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
-  { path: 'user', component: ProfileComponent },
+  { path: 'user', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: 'user/inventory/:userId', component: PaintsListComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard],
 })
 export class AppRoutingModule {}
