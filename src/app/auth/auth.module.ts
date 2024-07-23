1027ff6d42aecb96725ed5f0e2aeebfc3e4dac74
Nihler <vanilor97@gmail.com>
2024-07-23 16:17:23 +0200

    signup form rebuild with validators

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { AngularMaterialModule } from '../angular-material.module';
 import { AppRoutingModule } from '../app-routing.module';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
@@ -10,6 +10,12 @@ import { AuthRouterModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
-  imports: [CommonModule, AngularMaterialModule, FormsModule, AuthRouterModule],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    FormsModule,
+    AuthRouterModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AuthModule {}
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 import { AuthService } from '../auth.service';
+import { confirmPasswordValidator } from 'src/app/passwordMismatchValidator';
 
 @Component({
   selector: 'app-signup',
@@ -12,10 +13,34 @@ import { AuthService } from '../auth.service';
 export class SignupComponent implements OnInit, OnDestroy {
   isLoading = false;
   authStatusSub: Subscription;
+  signupForm: FormGroup;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    let emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    this.signupForm = new FormGroup(
+      {
+        login: new FormControl(null, {
+          validators: [Validators.required, Validators.minLength(3)],
+        }),
+        email: new FormControl(null, {
+          validators: [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.pattern(emailRegEx),
+          ],
+        }),
+        newPassword: new FormControl(null, {
+          validators: [Validators.required, Validators.minLength(4)],
+        }),
+        repeatPassword: new FormControl(null, {
+          validators: [Validators.required, Validators.minLength(4)],
+        }),
+      },
+      { validators: [confirmPasswordValidator] }
+    );
+
     this.authStatusSub = this.authService
       .getAuthStatusListener()
       .subscribe((authStatus) => {
