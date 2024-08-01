b5d57be9ea8b8911953cb1fff71721dea789d571
Nihler <vanilor97@gmail.com>
2024-08-01 15:29:22 +0200

    ui fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { CommonService } from './common.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  private authListenerSubs: Subscription;
   title = 'PaintsCatalogue';
   sidenavStatus: boolean = false;
   isLoggedIn = false;
@@ -26,9 +28,18 @@ export class AppComponent implements OnInit {
       this.sidenav.toggle();
     });
     this.isLoggedIn = this.authService.getIsAuth();
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuthenticated) => {
+        this.isLoggedIn = isAuthenticated;
+      });
     if (this.isLoggedIn) {
       this.username = this.authService.getUserName();
     }
+  }
+
+  ngOnDestroy(): void {
+    this.authListenerSubs.unsubscribe();
   }
 
   onLogout() {
